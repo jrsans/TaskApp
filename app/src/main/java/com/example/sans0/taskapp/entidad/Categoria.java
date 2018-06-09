471bda65fdff09f2aab42cf257fcec8e6cf89bf5
Junior <san07-10_@outlook.es>
2018-06-09 09:49:44 -0400

    Subida Proyecto

---
+++ app/src/main/java/com/example/sans0/taskapp/entidad/Categoria.java
@@ -0,0 +1,10 @@
+package com.example.sans0.taskapp.entidad;
+
+public class Categoria {
+    private Integer id;
+    private String descripcion;
+
+    public void setId(Integer id){
+        this.id= id;
+    }
+}
